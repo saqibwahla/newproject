4bd463eeee61795b41dfa7a5bd729cafc4055780
saqibwahla <iamsaqibwahla@gmail.com>
2020-09-08 09:01:17 -0700

    fdf

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<pizzaDelivery>'));
+app.use(express.static(__dirname + '/dist/<mypizzadelivery>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<pizzaDelivery>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<mypizzadelivery>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
